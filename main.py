c92ce5ac63f3ab08eb0a2176c285771890736132
Andres <afgaleano91@misena.edu.co>
2020-08-03 01:23:36 -0500

    se crea entorno virtual

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
+clients = 'andres,jose,'
+def create_client(client_name):
+	global clients
+
+	clients += client_name
+	_add_comma()
+
+
+def _add_comma():
+	global clients
+
+	clients += ','
+
+
+def list_client():
+	global clients
+
+	print(clients)
 
 
 if __name__ == '__main__':
-	pass
+	list_client()
+
+	create_client('pedro')
+
+	list_client()
+
