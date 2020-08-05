22c806c37aeb71f67e0a0902d45509494f717214
Andres <afgaleano91@misena.edu.co>
2020-08-04 21:43:04 -0500

    Se añade logica para el tema de mayus

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ clients = 'andres,jose,'
 def create_client(client_name):
 	global clients
 
-	clients += client_name
-	_add_comma()
+	if client_name not in clients:
+		clients += client_name
+		_add_comma()
+	else:
+		print('CLient already in in the client\'s list')
 
 
 def _add_comma():
@@ -17,11 +20,25 @@ def list_client():
 
 	print(clients)
 
+def _print_welcome():
+	print('WELCOME TO PLATZI VENTAS')
+	print('*' * 50)
+	print('What would you like to do today?')
+	print('[C]reate client')
+	print('[D]elete client')
+
 
 if __name__ == '__main__':
-	list_client()
+	_print_welcome()
 
-	create_client('pedro')
+	command = input('').upper()
 
-	list_client()
+	if command == 'C':
+		client_name = input('What is the client name? ')
+		create_client(client_name)
+		list_client()
+	elif command == 'D':
+		pass
+	else:
+		print('Invalid Command')
 
