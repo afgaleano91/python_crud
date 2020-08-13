9d533991bcf8bb61f6a67a733d1aaa172f7a763c
Andres <afgaleano91@misena.edu.co>
2020-08-13 00:58:38 -0500

    loops and conditional

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 clients = 'andres,jose,'
 def create_client(client_name):
 	global clients
@@ -20,12 +22,63 @@ def list_client():
 
 	print(clients)
 
+
+def update_client(client_name, updated_client_name):
+	global clients
+
+	if client_name in clients:
+		clients = clients.replace(client_name + ',', updated_client_name + ',')
+	else:
+		print('Client is not in clients list')
+
+
+def delete_client(client_name):
+	global clients
+	
+	if client_name in clients:
+		clients = clients.replace(client_name + ',', '')
+
+	else:
+		print('Client is not in clients list')
+
+
+def search_client(client_name):
+	global clients
+
+	client_list = clients.split(',')
+
+	for client in client_list:
+		if client != client_name:
+			continue
+		else:
+			return True
+
+
 def _print_welcome():
 	print('WELCOME TO PLATZI VENTAS')
 	print('*' * 50)
 	print('What would you like to do today?')
 	print('[C]reate client')
+	print('[L]ist client')
+	print('[U]pdate client')
 	print('[D]elete client')
+	print('[S]earch client')
+
+
+def _get_client_name():
+	client_name = None
+
+	while not client_name:
+		client_name =  input('What is the client name? ').lower()
+
+		if client_name == 'exit':
+			client_name = None
+			break
+		
+	if not client_name:
+			sys.exit()
+
+	return client_name
 
 
 if __name__ == '__main__':
@@ -34,11 +87,29 @@ if __name__ == '__main__':
 	command = input('').upper()
 
 	if command == 'C':
-		client_name = input('What is the client name? ')
+		client_name = _get_client_name()
 		create_client(client_name)
 		list_client()
+	elif command == 'L':
+		list_client()
 	elif command == 'D':
-		pass
+		client_name = _get_client_name()
+		delete_client(client_name)
+		list_client()
+	elif command == 'U':
+		client_name = _get_client_name()
+		updated_client_name = input('what is the updated client name: ')
+		update_client(client_name, updated_client_name)
+		list_client()
+	elif command == 'S':
+		client_name = _get_client_name()
+		found = search_client(client_name)
+
+		if found:
+			print('The client is in the client\'s list')
+		else:
+			print(f'The client: {client_name} is not in our client\'s list')
+
 	else:
 		print('Invalid Command')
 
