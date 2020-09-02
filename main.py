fabecaa0325afa524c031a161b2c565dc05f31f5
Andres <afgaleano91@misena.edu.co>
2020-09-01 23:25:20 -0500

    se permite guardar la informacion en disco

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 import sys
+import csv
+import os
 
-clients = [
-	{
-		'name': 'Pablo',
-		'company': 'Santa Monica',
-		'email': 'pablo@santamonica.com',
-		'position': 'Software Engineer'
-	},
 
-	{
-		'name': 'Ricardo',
-		'company': 'Bungie',
-		'email': 'Ricardo@Bungie.com',
-		'position': 'Designer'
-	}
+CLIENT_TABLE = '.clients.csv'
+CLIENT_SCHEMA = ['name', 'company', 'email', 'position']
+clients = []
 
-]
+def _initialize_clients_from_storage():
+	with open(CLIENT_TABLE, mode='r') as f:
+		reader = csv.DictReader(f, fieldnames=CLIENT_SCHEMA)
+
+		for row in reader:
+			clients.append(row)
+
+
+def _save_client_to_storage():
+	tmp_table_name = f'{CLIENT_TABLE}.tmp'
+	with open(tmp_table_name, mode='w') as f:
+		writer = csv.DictWriter(f, fieldnames=CLIENT_SCHEMA)
+		writer.writerows(clients)
+
+	os.remove(CLIENT_TABLE)
+	os.rename(tmp_table_name, CLIENT_TABLE)
+
+
 def create_client(client):
 	global clients
 
@@ -27,9 +36,11 @@ def create_client(client):
 
 def list_client():
 	global clients
-
+	print('uid |  name  | company  | email  | position ')
+	print('*' * 50)
+	
 	for idx, client in enumerate(clients):
-		print(f"{idx} : {client['name']}, {client['company']}, {client['email']}, {client['position']}")
+		print(f"{idx} : {client['name']} | {client['company']} | {client['email']} | {client['position']}")
 
 
 def update_client(new_client_name, updated_client_name):
@@ -39,7 +50,7 @@ def update_client(new_client_name, updated_client_name):
 	print(response)
 
 	if response["exists"]:
-		response["client"].update({'name': new_client_name})
+		response["client"].update(new_client_name)
 	# for client in clients:
 	# 	if updated_client_name in client['name']:
 	# 		client.update({'name': new_client_name})
@@ -96,7 +107,12 @@ def _get_client_name():
 	client_name = None
 
 	while not client_name:
-		client_name =  input('What is the client name? ').lower()
+		client_name = {
+			'name': _get_client_field('name'),
+			'company': _get_client_field('company'),
+			'email': _get_client_field('email'),
+			'position': _get_client_field('position'),
+		}
 
 		if client_name == 'exit':
 			client_name = None
@@ -109,6 +125,8 @@ def _get_client_name():
 
 
 if __name__ == '__main__':
+	_initialize_clients_from_storage()
+
 	_print_welcome()
 
 	command = input('').upper()
@@ -121,7 +139,6 @@ if __name__ == '__main__':
 			'position': _get_client_field('position')
 		}
 		create_client(client)
-		list_client()
 	elif command == 'L':
 		list_client()
 
@@ -134,13 +151,11 @@ if __name__ == '__main__':
 		else:
 			print("User not found")
 
-		list_client()
-
 	elif command == 'U':
 		updated_client_name = input('Search by name: ')
 		new_client_name = _get_client_name()
 		update_client(new_client_name, updated_client_name)
-		list_client()
+
 	elif command == 'S':
 		client_name = _get_client_name()
 		found = search_client(client_name)
@@ -153,3 +168,4 @@ if __name__ == '__main__':
 	else:
 		print('Invalid Command')
 
+	_save_client_to_storage()
