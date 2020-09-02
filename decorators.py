e95c7b69271eda10f4712e94ae64551f431c667b
Andres <afgaleano91@misena.edu.co>
2020-09-02 00:25:30 -0500

    uso de decoradores

--- decorators.py
+++ decorators.py
@@ -12,9 +12,26 @@ def password_required(func):
     
     return wrapper()
 
+
 @password_required
 def needs_password():
     print('La contraseña es correcta')
 
+
+def upper(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+
+        return result.upper()
+    
+    return wrapper
+
+
+@upper
+def say_my_name(name):
+    return (f'Hola {name}')
+
+
 if __name__ == '__main__':
-    needs_password()
+    # needs_password()
+    print(say_my_name("andres"))
