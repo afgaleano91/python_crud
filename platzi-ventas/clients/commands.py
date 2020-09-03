7045550f9fcc5d6018f61fb6f0e831c1676178a3
Andres <afgaleano91@misena.edu.co>
2020-09-03 01:36:04 -0500

    Se crea platzi ventas con framwork click solo modelo de crear

---
+++ platzi-ventas/clients/commands.py
@@ -0,0 +1,47 @@
+import click
+
+from clients.services import ClientService
+from clients.models import Client
+
+@click.group()
+def clients():
+    """ Manages the clients lifecycle """
+    pass
+
+
+@clients.command()
+@click.option('-n', '--name', type=str, prompt=True, help='The client name')
+@click.option('-n', '--company', type=str, prompt=True, help='The client company')
+@click.option('-n', '--email', type=str, prompt=True, help='The client email')
+@click.option('-n', '--position', type=str, prompt=True, help='The client position')
+@click.pass_context
+def create(ctx, name, company, email, position):
+    """ Creates a new client """
+    client_service = ClientService(ctx.obj['clients_table'])
+    client = Client(name, company, email, position)
+
+    client_service.create_client(client)
+
+
+@clients.command()
+@click.pass_context
+def list(ctx):
+    """ List all clients """
+    pass
+
+
+@clients.command()
+@click.pass_context
+def update(ctx, client_uid):
+    """ Updates a client """
+    pass
+
+
+@clients.command()
+@click.pass_context
+def delete(ctx, client_uid):
+    """ Deletes a client """
+    pass
+
+all = clients
+
